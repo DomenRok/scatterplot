8fc027baa77273d6d13777686d238717c813694d
DomenRok <bruncekdomenrok@gmail.com>
2019-03-20 21:21:20 +0100

    slowly climbing the learning curve

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,49 @@
-function yes(name: string) {
-    const ele = document.getElementById("greeting")
-    ele.innerHTML = name;
-}
+import * as d3 from 'd3';
+
+interface Coordinate {
+  x: number,
+  y: number,
+};
 
 
-yes("40500");
+var svg = d3.select("svg").attr("width", 600).attr("height", 600);
+
+let arrayOfObjects: Coordinate[] = [{x: 30, y:100}, {x:60, y:120}, {x:100, y:200}, {x: 120, y:240}];
+
+var scaleX = d3.scaleLinear()
+  .domain([10, 200])
+  .range([50, 550])
+  //.domain(d3.extent(arrayOfObjects.map(a => a.x)))
+var scaleY = d3.scaleLinear()
+  .domain([10, 200])  
+  .range([50, 550]);
+  //.domain(d3.extent(arrayOfObjects.map(a => a.y)))
+
+
+
+  
+function render(data: Coordinate[]) {
+  // Select
+  let circle = svg.selectAll("circle").data(data)
+  
+  // Enter
+  circle
+  .enter().append("circle")
+    .attr("cx", function(d) { return d.x; })
+    .attr("cy", function(d) { return d.y; })
+    .attr("r", 2.5)
+  
+    
+  circle
+    .attr("r", function(d) { return d.x/100 + Math.random() * 40});
+
+
+  // Exit
+  circle.exit().remove()
+};
+
+render(arrayOfObjects);
+
+d3.interval(function() {
+  render(arrayOfObjects);
+}, 500)
